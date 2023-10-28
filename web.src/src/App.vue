8b230d3457e375642139c607b722b8e7d0a75905
louiesun <louie.pingu@outlook.com>
2023-10-28 22:07:19 +0800

    origin
    Markdown Editor

---
+++ web.src/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <MyVditor/>
+</template>
+
+<script>
+import MyVditor from './components/MyVditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyVditor,
+  }
+}
+</script>
