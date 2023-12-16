5539c3df368dca0df9626a16b63b5a45758cdf04
louiesun <louie.pingu@outlook.com>
2023-12-16 17:56:09 +0800

    t

--- web.src/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue"
-import 'element-plus/dist/index.css'
-import App from "./App.vue"
-
-const app = createApp(App)
-app.mount("#app")
--- web.src/vite.config.js
+++ web.src/vite.config.js
@@ -1,49 +1,12 @@
-import {defineConfig} from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import ElementPlus from 'unplugin-element-plus/vite'
-import fs from "fs-extra"
-
-// auto copy vditor dist
-try {
-  fs.copySync('node_modules/vditor/dist', 'public/vditor-cdn/dist', {})
-  console.log('Copy vditor dist as local cdn success!')
-} catch (err) {
-  console.error(err)
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    ElementPlus({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  base: '/',
-
-  clearScreen: false,
-  envPrefix: ["VITE_"],
+  plugins: [vue()],
   build: {
     outDir: '../web/',
     emptyOutDir: true,
     target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "vue": ["vue"],
-          "vditor": ["vditor"],
-          "element-plus": ["element-plus"],
-        }
-      },
-    }
   },
-});
+})
